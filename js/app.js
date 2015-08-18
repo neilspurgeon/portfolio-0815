8f5e6201cb1548d5a7a32bec763bc3b76ba3d731
Neil <neilspurgeon@gmail.com>
2015-08-17 22:51:27 -0700

    footer working

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 $(function() {
-  var work = $('.work');
-  var topContainer = $('.main-content');
-  var topPosition = work.offset().top;
-  var docWindow = $(window);
-  var landing = $('.landing');
-  var footer = $('.site-footer');
+  var windowLoc = $(location).attr('pathname');
+  
+  switch(windowLoc){      
+    case "/":
+      homeParallax();
+      console.log("home");
+      break;
+    case "/alert.php":
+      break;
+  }
 
-  function topOfView() {
+  function homeParallax() {
+    var work = $('.work');
+    var topContainer = $('.main-content');
+    var topPosition = work.offset().top;
+    var docWindow = $(window);
+    var landing = $('.landing');
+    var footer = $('.site-footer');
+
+    footer.hide();
+
     docWindow.scroll(function () {
-      if (docWindow.scrollTop() > topPosition) {
+      if (docWindow.scrollTop() > (topPosition * 0.7)) {
         landing.hide(); 
         footer.show();
-      } else if (docWindow.scrollTop() <= topPosition) {
+      } else if (docWindow.scrollTop() <= (topPosition * 0.7)) {
         landing.show(); 
         footer.hide();
       }
     });
-  }
-
-  topOfView();
+  };
 
 });
